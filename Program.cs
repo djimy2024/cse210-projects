4ca0240759f43d4df40501571205d87e628a831b
Francillon2024 <fra24017@byui.edu>
2024-11-05 20:15:50 -0500

    Fixed problems where nothing was displayed on the screen.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Output a welcome message to the console
+            Console.WriteLine("Hello, World!");
+
+            // Prompt the user for their name
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            // Greet the user by their name
+            Console.WriteLine($"Hello, {name}!");
+
+            // Wait for the user to press a key before closing
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
