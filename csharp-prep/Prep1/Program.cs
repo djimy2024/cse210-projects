9f89dcbca9df151899d3bd915c19975dd17c92b0
Francillon2024 <fra24017@byui.edu>
2024-11-06 22:22:12 -0500

    Finish my prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,10 +6,10 @@ class Program
     {
         Console.WriteLine("Hello Prep1 World!");
         // Ask the user's first name.
-        Console.Write("What is your first name?");
+        Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
         // Ask the user's last name.
-        Console.Write("What is your last name?");
+        Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
         // Display the user's full name.
         Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
