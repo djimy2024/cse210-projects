8b18ae9b89715b1cf96ab1880474ce036bf4f787
Francillon2024 <fra24017@byui.edu>
2024-11-06 22:08:51 -0500

    Your commit message here

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -12,6 +12,6 @@ class Program
         Console.Write("What is your last name?");
         string last_name = Console.ReadLine();
         // Display the user's full name.
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}");
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
